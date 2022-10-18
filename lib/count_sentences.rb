88dc55e6f7225757beadae1c987a0699569f1b84
Noahkimtai <noahkimtai24@gmail.com>
2022-10-18 22:49:09 +0300

    Completed assignment

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
-
+    self.end_with?('?')
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    word_list = self.split(/[?.!]/)
+    word_list.delete('')
+    word_list.length()
   end
 end
+
+'This, well, is a sentence. This is too!! And so is this, I think? Woo...'.count_sentences
